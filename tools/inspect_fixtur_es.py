a387740a92885f4bc5b91bfb908eb7d2dcd89f7b
andypratt182 <63167693+andypratt182@users.noreply.github.com>
2026-08-10 10:36:07 +0100

    Modify inspect_fixtur_es.py to adjust sys.path
    Add sys.path manipulation to include parent directory

--- tools/inspect_fixtur_es.py
+++ tools/inspect_fixtur_es.py
@@ -1,4 +1,10 @@
-from collections import Counter, defaultdict
+import sys
+from pathlib import Path
+
+sys.path.insert(
+    0,
+    str(Path(__file__).resolve().parent.parent)
+)
 
 from sources.fixtur_es import get_all_fixtures
 
